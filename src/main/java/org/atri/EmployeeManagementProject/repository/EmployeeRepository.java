788c0b44d5451a14da331f68525f8091ae2210d8
unknown <atrinil@gmail.com>
2023-04-22 21:45:24 +0530

    added repository package

---
+++ src/main/java/org/atri/EmployeeManagementProject/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package org.atri.EmployeeManagementProject.repository;
+
+import java.util.List;
+
+import org.atri.EmployeeManagementProject.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
+	List<Employee> findByDept(String department);
+	
+	@Query(value = "select * from employee where dept =?1", nativeQuery = true)
+	List<Employee> findByDeptNative(String department);
+}
