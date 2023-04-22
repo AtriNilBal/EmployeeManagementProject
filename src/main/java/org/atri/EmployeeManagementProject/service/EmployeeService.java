88911319b9cf5de4abd5357787485b733e650653
unknown <atrinil@gmail.com>
2023-04-22 21:44:56 +0530

    added service interface

--- src/main/java/org/atri/EmployeeManagementProject/service/EmployeeService.java
+++ src/main/java/org/atri/EmployeeManagementProject/service/EmployeeService.java
@@ -1,5 +1,20 @@
 package org.atri.EmployeeManagementProject.service;
 
+import java.util.List;
+
+import org.atri.EmployeeManagementProject.entity.Employee;
+
 public interface EmployeeService {
 
+	Employee createEmployee(Employee newEmployee);
+	
+	Employee getById(long id);
+	
+	Employee updateEmployee(Employee modifiedEmployee);
+	
+	List<Employee> getAllEmployees();
+	
+	String deleteEmployeeById(long id);
+	
+	List<Employee> searchByDepartment(String dept);
 }
