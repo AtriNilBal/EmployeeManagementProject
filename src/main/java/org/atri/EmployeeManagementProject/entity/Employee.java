b079bbff0faf2046cf7642d4f8281d2ba61094ff
unknown <atrinil@gmail.com>
2023-04-22 21:44:16 +0530

    added Employee entity class

--- src/main/java/org/atri/EmployeeManagementProject/entity/Employee.java
+++ src/main/java/org/atri/EmployeeManagementProject/entity/Employee.java
@@ -1,5 +1,44 @@
 package org.atri.EmployeeManagementProject.entity;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private String dept;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 
 }
