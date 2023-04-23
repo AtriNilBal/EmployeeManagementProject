7c05d38be5d056d4e3eb8c81fc75dca6e4f59221
unknown <atrinil@gmail.com>
2023-04-23 17:37:52 +0530

    add swagger desc. of employee endpoints

--- src/main/java/org/atri/EmployeeManagementProject/controller/EmployeeController.java
+++ src/main/java/org/atri/EmployeeManagementProject/controller/EmployeeController.java
@@ -14,37 +14,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/employee")
+@Tag(name="Employee", description="APIs to perform create, read, search, update, delete actions on employee")
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
 	
 	@PostMapping("/create")
+	@Operation(description="create-employee", summary="Create new employee")
 	public Employee createEmployee(@RequestBody Employee newEmployee) {
 		Employee empCreated = this.employeeService.createEmployee(newEmployee);
 		return empCreated;
 	}
 	
 	@GetMapping("/")
+	@Operation(description="get-all-employees", summary="Get all employees")
 	public List<Employee> getAllEmployees() {
 		return this.employeeService.getAllEmployees();
 	}
 	
 	@GetMapping("/{id}")
+	@Operation(description="get-employee-by-id", summary="Get employee by id")
 	public Employee getById(@PathVariable long id) {
 		Employee emp = this.employeeService.getById(id);
 		return emp;
 	}
 	
 	@PutMapping("/modify")
+	@Operation(description="modify-employee", summary="Update record of existing employee")
 	public Employee updateEmployee(@RequestBody Employee modifiedEmployee) {
 		Employee emp = this.employeeService.updateEmployee(modifiedEmployee);
 		return emp;
 	}
 	
 	@DeleteMapping("/{id}")
+	@Operation(description="delete-employee", summary="delete employee by id")
 	public String deleteEmployee(@PathVariable long id) {
 		return this.employeeService.deleteEmployeeById(id);
 	}
